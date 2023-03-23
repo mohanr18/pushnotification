b0137ba2e17c7e92ffd3835c9d076dc70fc775c4
mohanraj <Mohanraj.J@saint-gobain.com>
2023-03-23 11:39:59 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDhMTKyeDmDa5thm26MGljfzkVl5sncs7Y",
-    authDomain: "glassdirect-2.firebaseapp.com",
-    projectId: "glassdirect-2",
-    storageBucket: "glassdirect-2.appspot.com",
-    messagingSenderId: "54723755585",
-    appId: "1:54723755585:web:3467c972d0c037de7050c0",
-    measurementId: "G-D607NPNSSD",
-    vapidKey: "BLHodb8lGghdO-u2rJfRnkcO5UtFyF32irya9zwL6NT_NZse4atUDlT0E_3R67vRtM9xOqpYXpyBZjsfljEO0Mo",
+    apiKey: "AIzaSyCPyuJpEvej939uPL76hyLv3929AWsHv1k",
+authDomain: "glasspushnotification.firebaseapp.com",
+ projectId: "glasspushnotification",
+ storageBucket: "glasspushnotification.appspot.com",
+ messagingSenderId: "239757467941",
+ appId: "1:239757467941:web:d78f0ef9cf5601eb577acc",
+ measurementId: "G-JNEHD8ZPJP",
+    vapidKey: "BIaWdOYoYN3VqCdFgRZHLY2t4xpbNxfr02_1Dsac2-Zl-wwg8XziH2vieotfSclHnHV_1tePfXWH1GuxtozIe3g",
   },
 };
 
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,11 +1,11 @@
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 firebase.initializeApp({
-  apiKey: "AIzaSyDhMTKyeDmDa5thm26MGljfzkVl5sncs7Y",
-   authDomain: "glassdirect-2.firebaseapp.com",
-    projectId: "glassdirect-2",
-     storageBucket: "glassdirect-2.appspot.com",
-      messagingSenderId: "54723755585",
-      appId: "1:54723755585:web:3467c972d0c037de7050c0",
-      smeasurementId: "G-D607NPNSSD"
+  apiKey: "AIzaSyCPyuJpEvej939uPL76hyLv3929AWsHv1k",
+authDomain: "glasspushnotification.firebaseapp.com",
+ projectId: "glasspushnotification",
+ storageBucket: "glasspushnotification.appspot.com",
+ messagingSenderId: "239757467941",
+ appId: "1:239757467941:web:d78f0ef9cf5601eb577acc",
+ measurementId: "G-JNEHD8ZPJP"
   });
